a9602aa14abc8534613eeff350c29dd469f266c7
barwnik <jacek_stopa@hotmail.com>
2023-03-30 17:51:11 -0600

    Create Asterisk_Pyramid.cs

---
+++ Asterisk_Pyramid.cs
@@ -0,0 +1,26 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int rows = 10; // Change this value to adjust the height of the pyramid
+        int columns = rows * 2 - 1;
+
+        for (int i = 1; i <= rows; i++)
+        {
+            for (int j = 1; j <= columns; j++)
+            {
+                if (j >= rows - i + 1 && j <= rows + i - 1)
+                {
+                    Console.Write("*");
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
